db4722851783abf99629f03344dce9d4bc042640
stockinemma <stockinemma@gmail.com>
2017-08-04 23:11:07 -0400

    Added weather-app

---
+++ weather-app/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+import xhr from 'xhr';
+
+class App extends React.Component {
+  state = {
+    location: '',
+    data: {}
+  };
+
+   fetchData = (evt) => {
+    evt.preventDefault();
+
+    var location = encodeURIComponent(this.state.location);
+
+    var urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    var urlSuffix = '&APPID=166a433c57516f51dfab1f7edaed8413&units=metric';
+    var url = urlPrefix + location + urlSuffix;
+
+    var self = this;
+
+    xhr({
+      url: url
+    }, function (err, data) {
+      self.setState({
+        data: JSON.parse(data.body)
+      });
+    });
+  };
+
+  changeLocation = (evt) => {
+    this.setState({
+      location: evt.target.value
+    });
+  };
+
+  render() {
+    var currentTemp = 'not loaded yet';
+    if (this.state.data.list) {
+      currentTemp = this.state.data.list[0].main.temp;
+    }
+     return (
+      <div>
+        <h1>Weather</h1>
+        <form onSubmit={this.fetchData}>
+          <label>I want to know the weather for
+            <input
+              placeholder={"City, Country"}
+              type="text"
+              value={this.state.location}
+              onChange={this.changeLocation}
+            />
+          </label>
+        </form>
+        <p className="temp-wrapper">
+          <span className="temp">{ currentTemp }</span>
+          <span className="temp-symbol">°C</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
